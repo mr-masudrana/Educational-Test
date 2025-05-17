89a0b27b3ecfb6d93ca6eda4734ba7b407eb8bfc
Masud Rana <71147447+mr-masudrana@users.noreply.github.com>
2025-05-17 22:21:56 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import requests
+import datetime
 
-channels = [
+playlist = [
     {
         "name": "TOFFEE Sports VIP",
         "link": "https://bldcmprod-cdn.toffeelive.com/cdn/live/sports_highlights/playlist.m3u8",
@@ -18,41 +18,10 @@ channels = [
     }
 ]
 
-headers = {
-    "User-Agent": "Toffee (Linux;Android 14) AndroidXMedia3/1.1.1/64103898/4d2ec9b8c7534adc",
-    "Origin": "https://toffeelive.com",
-    "Referer": "https://toffeelive.com/",
-    "Accept": "*/*"
-}
-
-m3u_data = "#EXTM3U\n"
-
-for ch in channels:
-    print(f"[*] Processing: {ch['name']}")
-    try:
-        r = requests.get(ch["link"], headers=headers)
-        cookie_header = r.headers.get("set-cookie", "")
-        edge_cookie = ""
-
-        for c in cookie_header.split(";"):
-            if "Edge-Cache-Cookie" in c:
-                edge_cookie = c.strip()
-                break
-
-        if not edge_cookie:
-            print(f"[!] Edge-Cache-Cookie not found for {ch['name']}, skipping.")
-            continue
-
-        m3u_data += f'#EXTINF:-1 tvg-id="{ch["name"]}" tvg-logo="{ch["logo"]}",{ch["name"]}\n'
-        m3u_data += f'#EXTVLCOPT:http-referrer=https://toffeelive.com/\n'
-        m3u_data += f'#EXTVLCOPT:http-user-agent={headers["User-Agent"]}\n'
-        m3u_data += f'#EXTVLCOPT:http-cookie={edge_cookie}\n'
-        m3u_data += f'{ch["link"]}\n\n'
-
-    except Exception as e:
-        print(f"[!] Error for {ch['name']}: {e}")
-
 with open("toffee_playlist.m3u", "w", encoding="utf-8") as f:
-    f.write(m3u_data)
+    f.write("#EXTM3U\n")
+    for channel in playlist:
+        f.write(f'#EXTINF:-1 tvg-logo="{channel["logo"]}",{channel["name"]}\n')
+        f.write(f'{channel["link"]}\n')
 
-print("[✓] toffee_playlist.m3u created successfully.")
+print("Playlist updated:", datetime.datetime.now())
